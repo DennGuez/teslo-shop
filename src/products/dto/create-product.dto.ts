4e76b0930a5cde117b0c2cb8f08786c05bdf02ae
Dennis Rodriguez <dennisrodriguezx@gmail.com>
2023-02-23 00:31:39 -0400

    FEAT: Product DTO's Added

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,35 @@
+import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength } from "class-validator"
+
+export class CreateProductDto {
+
+    @IsString()
+    @MinLength(1)
+    title: string
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number
+
+    @IsString()
+    @IsOptional()
+    description?: string
+
+    @IsString()
+    @IsOptional()
+    slug?: string
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    stock?: number
+
+    /* each hace que cada elemento del Array cumpla la regla de ser un string */
+    @IsString({each: true})
+    @IsArray()
+    sizes: string[]
+
+    @IsIn(['men', 'women', 'kid', 'unisex'])
+    gender: string
+
+}
